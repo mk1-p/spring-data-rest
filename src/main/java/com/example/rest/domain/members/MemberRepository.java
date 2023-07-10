0bfd95f8a79e5403ddbf4fb69793681b0c0c2ac3
mk1-p <mk1.dev662@gmail.com>
2023-07-10 18:20:42 +0900

    CLN: 오기입 수정

--- src/main/java/com/example/rest/domain/members/MemberRepository.java
+++ src/main/java/com/example/rest/domain/members/MemberRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.Repository;
 
 
-public interface MemberRepository extends JpaRepository<Member, Repository> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
