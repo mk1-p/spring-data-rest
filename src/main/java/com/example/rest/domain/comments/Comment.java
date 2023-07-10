7e79c050ab2ea1015acd12c623b41b1df9de73b7
mk1-p <mk1.dev662@gmail.com>
2023-07-10 17:22:58 +0900

    ENH: Comment 추가

---
+++ src/main/java/com/example/rest/domain/comments/Comment.java
@@ -0,0 +1,28 @@
+package com.example.rest.domain.comments;
+
+
+import com.example.rest.domain.boards.Board;
+import com.example.rest.domain.members.Member;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "text", length = 180)
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id",referencedColumnName = "id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id",referencedColumnName = "id")
+    private Board board;
+
+}
